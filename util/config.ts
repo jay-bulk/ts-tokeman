a14fe66b35b7ec5de1ae7543609df88bfc63e058
jay-bulk <rbulkley@byu.edu>
2023-06-10 15:43:08 -0600

    feat: environment loader

--- util/config.ts
+++ util/config.ts
@@ -1,35 +1,21 @@
 import fs from "fs"
 import path from "path"
 import { ux } from '@oclif/core'
-import { configDotenv } from 'dotenv'
-
-export async function setConfigDir(): Promise<void> {
+import { Environment } from './env'
+export async function writeConfigDir(): Promise<void> {
 
     let antwort  = false;
     let configDir = await ux.prompt('Where should we store client-ids?')
     antwort = await ux.confirm(`${configDir} selected.\nIs that correct?`)
 
-    if (!antwort) {setConfigDir()}
-
-    let configuration = {
-        configDir: this.configDir,
-        defaultEnv: 'dev',
-        apiIssuer: configDotenv().parsed.API_ISSUER ?? 'https://postman-echo.com/token',
-        jwtProvider: configDotenv().parsed.GATEWAY_URL ?? 'https://postman-echo.com/post'
-    }
+    if (!antwort) { writeConfigDir() }
 
     try {
-        fs.writeFile('tokeman.json', JSON.stringify(configuration) )
-        this.log(`Configuration file set to ${path.extname(configDir)}`)
+        fs.writeFile('tokeman.json', JSON.stringify(Environment.config_dir) )
+        this.log(`Configuration file set to ${configDir}`)
     } catch (e) {
         this.log('Failed to write')
         this.log(e)
         process.exit()
     }
-}
-
-export function getConfigDir(): Promise<string> {
-    let configDir
-
-    return configDir
 }
